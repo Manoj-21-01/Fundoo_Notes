9f549818866a31bc4947e5a981baf98198420654
Manoj Veluru <manojveluru21@gmail.com>
2024-01-25 18:18:22 +0530

    project

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -2,7 +2,10 @@ import Joi from '@hapi/joi';
 
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(4).required()
+    first_name: Joi.string().min(4).required(),
+    last_name: Joi.string().optional(),
+    emailid:Joi.string().min(6).required(),
+    password:Joi.string().min(8).required()
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
