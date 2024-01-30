b868a4f76f0fe52b5c6ba0a6bc28ab9ad10c7870
Manoj Veluru <manojveluru21@gmail.com>
2024-01-30 10:44:00 +0530

    [MANOJ] Added: Modified files

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -1,12 +1,6 @@
 import Note from '../models/note.model';
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
-//get all notes
-
-// export const getAllNotes = async () => {
-//   const data = await Note.find();
-//   return data;
-// };
 
 // create new note
 export const newNote = async (body) => {
@@ -14,22 +8,8 @@ export const newNote = async (body) => {
     return data;
   };
 
-
-
 // login
 export const loginNote = async (body) => {
   // Finding the note with the given email
   const note = await Note.findOne({ title: body.title  });
-  if (note) {
-
-  // Comparing the entered password with the hashed password in the database
-  const passwordMatch = await bcrypt.compare(body.description, note.description);
-  if (passwordMatch) {
-    var token = jwt.sign({ emailid: 'naniguy1@gmail.com' }, 'shhhhh');
-    return token;
-  }
-  else {throw new Error ('Invalid password')};
-}
-else {throw new Error ('Invalid emailid')};
-
 };
